e1536e349a8862bb0d91438be1b5621f5d4bb7e9
valzate <valzate223@gmail.com>
2025-08-08 19:24:09 -0500

    se crea una clase para la cola

--- cola.js
+++ cola.js
@@ -4,3 +4,50 @@ class nodo{
         this.siguiente = null;
     }
 }
+class cola{ 
+    constructor() {
+        this.frente = null;
+        this.final = null;
+        this.tamaño = 0;
+    }
+    enqueue(dato) { //Funcion que agrega un elemento a la cola
+        let nuevoNodo = new nodo(dato);
+        if (this.final == null) {
+            this.frente = nuevoNodo;
+            this.final = nuevoNodo;
+        } else {
+            this.final.siguiente = nuevoNodo;
+            this.final = nuevoNodo;
+        }
+        this.tamaño++;
+    }
+    dequeue() { //Funcion que elimina el elemento en este caso el primero en entrar
+        if (this.frente == null) {
+            return null;
+        } else {
+            let dato = this.frente.dato;
+            this.frente = this.frente.siguiente;
+            if (this.frente == null) {
+                this.final = null;
+            }
+            this.tamaño--;
+            return dato;
+        }
+    }
+    peek() { //Funcion que retorna el elemento al frente de la cola sin eliminarlo
+        if (this.frente == null) {
+            return null;
+        } else {
+            return this.frente.dato;
+        }
+    }
+    print() { //Funcion que imprime los elementos de la cola
+        let aux = this.frente;
+        let resultado = "";
+        while (aux != null) {
+            resultado += aux.dato + " ";
+            aux = aux.siguiente;
+        }
+        console.log(resultado);
+    }
+}
